b0cca0fa978063ec4e04646e1c64aa99349adfac
Powerkrieger <81979050+Powerkrieger@users.noreply.github.com>
2023-12-02 11:28:49 +0100

    day 1 part 2

--- 2023/day01.py
+++ 2023/day01.py
@@ -1,9 +1,28 @@
+def starting_here(word, numbers):
+    for i, number in enumerate(numbers):
+        if word.startswith(number):
+            return i
+    return None
+
+
 if __name__ == "__main__":
     with open("input01.txt") as file:
         df = file.readlines()
 
+        numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
         sum = 0
         for line in df:
+            print(line)
+            to_be_inserted = []
+            for i in range(len(line)):
+                word = line[i:]
+                number = starting_here(word, numbers)
+                if number is not None:
+                    to_be_inserted.append((i, number))
+            for count, (index, number) in enumerate(to_be_inserted, start=0):
+                line = line[:index + count] + str(number) + line[index + count:]
+
             print(line)
             rev = line[::-1]
 
