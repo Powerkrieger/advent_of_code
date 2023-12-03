b5e18c78e3d502f422770acddc739953c505f7c1
Powerkrieger <81979050+Powerkrieger@users.noreply.github.com>
2023-12-03 13:17:09 +0100

    day 3 part 2

--- 2023/day03.py
+++ 2023/day03.py
@@ -7,6 +7,8 @@ def check_if_true(matrix, mask):
     print(len(matrix))
     print(mask.shape)
     result = 0
+    gear_score = 0
+    star_dict = {}
     for x, line in enumerate(matrix, start=0):
 
         counter = 0
@@ -17,11 +19,12 @@ def check_if_true(matrix, mask):
                 only_numbers.append(int(letter))
         print(only_numbers)
         number_flag = False
+
         for y, letter in enumerate(line, start=0):
             if letter.isdigit():
-                print(f"x:{x}, y:{y}")
-                if mask[x][y]:
-                    result += only_numbers[counter]
+                if mask[x][y] > 0:
+                    if only_numbers[counter] > 0:
+                        star_dict.setdefault(mask[x][y], []).append(only_numbers[counter])
                     only_numbers[counter] = 0
                 number_flag = True
             elif letter == "." and number_flag is True:
@@ -32,7 +35,10 @@ def check_if_true(matrix, mask):
                 counter += 1
                 number_flag = False
                 continue
-    return result
+    for key in star_dict.keys():
+        if len(star_dict[key]) == 2:
+            gear_score += star_dict[key][0] * star_dict[key][1]
+    return gear_score
 
 
 if __name__ == "__main__":
@@ -43,10 +49,8 @@ if __name__ == "__main__":
         special_characters = []
         digit_mask = []
         special_characters_mask = []
-
         neighbors = [(-1, -1), (0, -1), (1, -1), (-1, 0), (0, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]
-
-        print(len(df))
+        star_id = 1
         for i, line in enumerate(df):
             line = line[:-1]
             df[i] = line
@@ -55,7 +59,8 @@ if __name__ == "__main__":
                 if not x.isdigit() and x != '.' and x != '\n' and not x in special_characters:
                     special_characters.append(x)
             digit_mask.append([1 if x.isdigit() else 0 for x in line])
-            special_characters_mask.append([1 if x in special_characters else 0 for x in line])
+            special_characters_mask.append([idx if x == "*" else 0 for idx, x in enumerate(line, start=star_id)])
+            star_id += len(special_characters_mask[i])
 
         special_characters_mask = np.array(special_characters_mask)
         new_special_characters_mask = np.zeros(
@@ -65,13 +70,12 @@ if __name__ == "__main__":
             for y in range(special_characters_mask.shape[1]):
                 for n in neighbors:
                     try:
-                        if special_characters_mask[x + n[0]][y + n[1]] == 1:
-                            new_special_characters_mask[x][y] = 1
+                        if special_characters_mask[x + n[0]][y + n[1]] > 0:
+                            new_special_characters_mask[x][y] = special_characters_mask[x + n[0]][y + n[1]]
                             break
                     except IndexError:
                         continue
         digit_mask = np.array(digit_mask)
-        mask = (digit_mask == 1) & (new_special_characters_mask == 1)
-        answer = check_if_true(df, mask)
+        answer = check_if_true(df, new_special_characters_mask)
 
         print(f"answer {answer}")
