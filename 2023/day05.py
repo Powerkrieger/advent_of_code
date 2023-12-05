6dce37bba0b90aaee6ea915cf288e33088397ea8
Powerkrieger <81979050+Powerkrieger@users.noreply.github.com>
2023-12-05 12:50:09 +0100

    day 5 part 2 --- 0.00726318359375 seconds ---

--- 2023/day05.py
+++ 2023/day05.py
@@ -1,42 +1,94 @@
 import sys
+import time
 
 
-def dings(start, df, i):
-    goal = start
+def transfer_to_dest(source, val, dest):
+    return dest + val - source
+
+
+def dings_range(orig_ranges, df, i):
+
+    new_ranges = []
     while df[i] != '\n' and df[i] is not None:
         dest, source, range_of = [int(x) for x in df[i][:-1].split(" ")]
-        if source < start < (source + range_of):
-            goal = dest + start - source
+        source = (source, source + range_of -1)
+        # print(f"s_range {source}, {dest - source[0]}")
+        old_ranges = []
+        for o_range in orig_ranges:
+            # print(f"o_range {o_range}")
+            # no overlap , range before
+            if o_range[1] < source[0]:
+                old_ranges.append(o_range)
+            # no overlap, range after
+            elif o_range[0] > source[1]:
+                old_ranges.append(o_range)
+            # only overlap
+            elif o_range[0] >= source[0] and o_range[1] <= source[1]:
+                new_ranges.append(
+                    (transfer_to_dest(source[0], o_range[0], dest), transfer_to_dest(source[0], o_range[1], dest)))
+            # overlap both sides
+            elif o_range[0] < source[0] and o_range[1] > source[1]:
+                old_ranges.append((o_range[0], source[0] - 1))
+                new_ranges.append((transfer_to_dest(source[0], source[0], dest), transfer_to_dest(source[0], source[1], dest)))
+                old_ranges.append((source[1]+1, o_range[1]))
+            # overlap front
+            elif source[0] <= o_range[1] <= source[1]:
+                old_ranges.append((o_range[0], source[0] - 1))
+                new_ranges.append((transfer_to_dest(source[0], source[0], dest), transfer_to_dest(source[0], o_range[1], dest)))
+            # overlap back
+            elif source[0] <= o_range[0] <= source[1]:
+                new_ranges.append((transfer_to_dest(source[0], o_range[0], dest), transfer_to_dest(source[0], source[1], dest)))
+                old_ranges.append((source[1]+1, o_range[1]))
+            else:
+                print("dumbass")
+                raise Exception
         i += 1
+        orig_ranges = old_ranges
+    for x in orig_ranges:
+        new_ranges.append(x)
     i += 2
-    return goal, i
+    return new_ranges, i
 
 
-if __name__ == "__main__":
+def main():
     with open("inputs/input05.txt") as file:
         df = file.readlines()
         answer = sys.maxsize
         line = df[0][7:-1]
         seeds = [int(seed) for seed in line.split(" ")]
+        seed_ranges = []
+        i = 1
+        while i < len(seeds):
+            seed_ranges.append((seeds[i - 1], seeds[i - 1] + seeds[i] - 1))
+            i += 2
 
-        for seed in seeds:
+        for seed_range in seed_ranges:
             i = 3
-            print(seed)
-            soil, i = dings(seed, df, i)
-            print(soil)
-            fertilizer, i = dings(soil, df, i)
-            print(fertilizer)
-            water, i = dings(fertilizer, df, i)
-            print(water)
-            light, i = dings(water, df, i)
-            print(light)
-            temperature, i = dings(light, df, i)
-            print(temperature)
-            humidity, i = dings(temperature, df, i)
-            print(humidity)
-            location, i = dings(humidity, df, i)
-            print(location)
-            if location < answer:
-                answer = location
+            seed = [seed_range]
+            print(f"seed range {seed}")
+            soil, i = dings_range(seed, df, i)
+            print(f"soil range {soil}")
+            fertilizer, i = dings_range(soil, df, i)
+            print(f"fertilizer range {fertilizer}")
+            water, i = dings_range(fertilizer, df, i)
+            print(f"water range {water}")
+            light, i = dings_range(water, df, i)
+            print(f"light range {light}")
+            temperature, i = dings_range(light, df, i)
+            print(f"temperature range {temperature}")
+            humidity, i = dings_range(temperature, df, i)
+            print(f"humidity range {humidity}")
+            location, i = dings_range(humidity, df, i)
+            print(f"location range {location}")
+            for some_range in location:
+                print(some_range)
+                if some_range[0] < answer:
+                    answer = some_range[0]
             print("-----------\n")
         print(f"answer: {answer}")
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    main()
+    print("--- %s seconds ---" % (time.time() - start_time))
