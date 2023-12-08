5f8598b99633b45e9c0d37144f56934322853b59
Powerkrieger <81979050+Powerkrieger@users.noreply.github.com>
2023-12-08 12:50:24 +0100

    day 8 part 2 (2023) --- 0.035 seconds ---

--- 2023/day08.py
+++ 2023/day08.py
@@ -1,31 +1,44 @@
+import math
 import time
+
+
+def solve(node, network, lr_sequence):
+    x = node
+    seq_i = 0
+    seq_length = len(lr_sequence)
+    count = 0
+    while x[2] != "Z":
+        seq_i = seq_i % seq_length
+        # print(network[x][lr_sequence[seq_i]])
+        try:
+            x = network[x][lr_sequence[seq_i]]
+        except Exception:
+            print(seq_i)
+            raise Exception
+        count += 1
+        seq_i += 1
+    return count
 
 
 def main():
     with open("inputs/input08.txt") as file:
         puzzle = file.readlines()
-        answer = 0
-
         lr_sequence = [0 if direction == "L" else 1 for direction in puzzle[0][:-1]]
-
+        answer = []
         network = {}
+        starting_nodes = []
         for node in puzzle[2:]:
             network[node[:3]] = (node[7:10], node[12:15])
+            if node[2] == "A":
+                starting_nodes.append(node[:3])
 
-        print(network)
+        for node in starting_nodes:
+            answer.append(solve(node, network, lr_sequence))
 
-        x = "AAA"
-        seq_i = 0
-        seq_length = len(lr_sequence)
+        print(answer)
+        print(*answer)  # *mind blown*
 
-        while x != "ZZZ":
-            seq_i = seq_i % seq_length
-            print(network[x][lr_sequence[seq_i]])
-            x = network[x][lr_sequence[seq_i]]
-            answer += 1
-            seq_i += 1
-
-        print(f"answer: {answer}")
+        print(f"answer: {math.lcm(*answer)}")
 
 
 if __name__ == "__main__":
