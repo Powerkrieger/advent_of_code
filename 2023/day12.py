6a4698722d5296d25db646321ff060d5bc050600
Powerkrieger <81979050+Powerkrieger@users.noreply.github.com>
2023-12-12 14:45:08 +0100

    day 12 part 1 (2023) --- 0.148 seconds --- updated

--- 2023/day12.py
+++ 2023/day12.py
@@ -1,41 +1,58 @@
 import time
 
-import numpy as np
+
+def can_position_group_here(starting_position, group_size, springs, max_length):
+    if starting_position > 0 and springs[starting_position-1] == 1:
+        return False
+    if starting_position + group_size > max_length:
+        return False
+    if starting_position + group_size < max_length and springs[starting_position+group_size] == 1:
+        return False
+    for i in range(group_size):
+        if springs[starting_position + i] > 0:
+            continue
+        return False
+    return True
 
 
-def generate_groups(perms, springs, number_of_twos):
-    groups = []
-    for i in range(perms):
-        new_springs = []
-        perm_counter = 0
-        binary = f"{i:b}".zfill(number_of_twos)
-        for letter in springs:
-            if letter > 1:
-                new_springs.append(int(binary[perm_counter]))
-                perm_counter += 1
-            else:
-                new_springs.append(letter)
-        groups.append(new_springs)
-    return groups
+def no_hashtags_left(starting_position, springs, max_length):
+    for i in range(starting_position, max_length):
+        if springs[i] == 1:
+            return False
+    return True
 
 
-def count_groups(group):
-    result_groups = []
-    group_counter = 0
-    group_flag = False
-    for i in group:
-        if i == 1 and group_flag:
-            group_counter += 1
-        elif i == 1:
-            group_counter = 1
-            group_flag = True
-        elif i == 0 and group_flag:
-            group_flag = False
-            result_groups.append(group_counter)
-            group_counter = 0
-    if group_flag:
-        result_groups.append(group_counter)
-    return result_groups
+def hashtags_missed(starting_position, end, springs):
+    for i in range(starting_position, end):
+        if springs[i] == 1:
+            return True
+    return False
+
+
+def recursive_function(starting_position, numbers, springs, max_length):
+    answer = 0
+    if len(numbers) <= 0 and no_hashtags_left(starting_position, springs, max_length):
+        return 1
+    elif len(numbers) <= 0:
+        pass
+    elif starting_position >= max_length:
+        pass
+    else:
+        for i in range(starting_position, max_length):
+            if hashtags_missed(starting_position, i, springs):
+                break
+            elif len(numbers) <= 0 and no_hashtags_left(starting_position, springs, max_length):
+                return answer + 1
+            elif len(numbers) <= 0:
+                break
+            elif can_position_group_here(i, numbers[0], springs, max_length):
+                copy_numbers = numbers.copy()
+                g = copy_numbers.pop(0)
+                new_starting_position = i + g + 1
+                answer += recursive_function(new_starting_position, copy_numbers, springs, max_length)
+            elif springs[i] == 1:
+                break
+    return answer
 
 
 def main():
@@ -47,20 +64,8 @@ def main():
             numbers = [int(x) for x in numbers.split(",")]
             springs = [0 if v == "." else 1 if v == "#" else 2 for v in springs]
 
-            unknowns = springs.count(2)
-            perms = 2**unknowns
-
-            print("===============================")
-            print(f"permutations to check: {perms}")
-
-            groups_to_check = generate_groups(perms, springs, unknowns)
-
-            permutations = 0
-            for group in groups_to_check:
-                check = count_groups(group)
-                if np.array_equal(numbers, check):
-                    permutations += 1
-
+            max_length = len(springs)
+            permutations = recursive_function(0, numbers, springs, max_length)
             print(f"working permutations for this run: {permutations}")
             answer += permutations
 
