f42238ac0a294f0030d8fe90d0a2cffc9c0c59da
Powerkrieger <81979050+Powerkrieger@users.noreply.github.com>
2023-12-13 16:50:19 +0100

    day 13 part 1 (2023) --- 0.028 seconds --- updated for simplicity

--- 2023/day13.py
+++ 2023/day13.py
@@ -1,70 +1,50 @@
 import time
+
 import numpy as np
 
 
-def recheck(pattern, count_rows, starting_index):
-    if starting_index * 2 < count_rows - 1:
-
-        for i, x in zip(range(starting_index, -1, -1), range(1, starting_index*2 + 2, 2)):
-            if not np.array_equal(pattern[i], pattern[i + x]):
-                return False
-        return True
-    else:
-        for i, x in zip(range(starting_index+1, count_rows), range(1, starting_index*2 + 1, 2)):
-            if not np.array_equal(pattern[i], pattern[i - x]):
-                return False
-        return True
+def check_for_mirror(i, pat):
+    row_length = len(pat)
+    # for 0 - number of rows - 2
+    mirror = []
+    for up, down in zip(range(i, -1, -1), range(i+1, row_length, 1)):
+        mirror.append(up)
+        mirror.append(down)
+        if not np.array_equal(pat[up], pat[down]):
+            return []
+    return mirror
 
 
-def compute(pattern, count_rows):
-    result = 0
-    count_cols = len(pattern[0])
-    turned_pattern = list(zip(*pattern))
-    if count_rows < count_cols:
-        # find mirror horizontally first
-        for i in range(count_rows - 1):
-            if np.array_equal(pattern[i], pattern[i + 1]):
-                if recheck(pattern, count_rows, i):
-                    return 100 * (i+1)
-        # else find vertical mirror
-        for i in range(count_cols - 1):
-            if np.array_equal(turned_pattern[i], turned_pattern[i + 1]):
-                if recheck(turned_pattern, count_cols, i):
-                    return (i+1)
-    else:
-        # find mirror vertically first
-        for i in range(count_cols - 1):
-            if np.array_equal(turned_pattern[i], turned_pattern[i + 1]):
-                if recheck(turned_pattern, count_cols, i):
-                    return (i+1)
-        # find mirror horizontally
-        for i in range(count_rows - 1):
-            if np.array_equal(pattern[i], pattern[i + 1]):
-                if recheck(pattern, count_rows, i):
-                    return 100 * (i+1)
+def compute(pattern):
 
-    return result
+    for multiply_by, pat in [(100, pattern), (1, list(zip(*pattern)))]:
+        row_length = len(pat)
+        # for 0 - number of rows - 2
+        for i in range(row_length - 1):
+            if np.array_equal(pat[i], pat[i+1]):
+                # possibly mirror
+                if check_for_mirror(i, pat):
+                    return multiply_by * (i+1)
 
 
 def main():
     with open("inputs/input13.txt") as file:
         puzzle = file.readlines()
         answer = 0
-
         count_rows = 0
         pattern = []
         for line in puzzle:
             if line == "\n":
-                result = compute(pattern, count_rows)
+                result = compute(pattern)
                 print(f"result {result}")
                 answer += result
                 count_rows = 0
                 pattern = []
             else:
-                pattern.append(line[:-1])
+                pattern.append([0 if x == "." else 1 for x in line[:-1]])
                 count_rows += 1
         if count_rows > 0:
-            result = compute(pattern, count_rows)
+            result = compute(pattern)
             print(f"result {result}")
             answer += result
 
