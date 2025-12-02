a1f91acf01ecbff418e6b2f2f3c1655101df4a4f
joost <joost.rehder@uni-rostock.de>
2025-12-02 06:29:08 +0100

    part 2 day 2,

--- 2025/day_02.py
+++ 2025/day_02.py
@@ -1,9 +1,9 @@
-def is_invalid(number):
-    if len(str(number)) % 2 == 0:
-        start = int(str(number)[len(str(number))//2:])
-        end = int(str(number)[:len(str(number))//2])
-        if start == end:
-            return True
+def is_invalid(number, number_of_chars):
+    for i in range(1, number_of_chars):
+        if len(str(number)) % i == 0:
+            parts = [str(number)[j:j+i] for j in range(0, len(str(number)), i)]
+            if all(str(part) == str(parts[0]) for part in parts):
+                return True
     return False
 
 with open("inputs/input_day_02.txt", "r+") as file1:
@@ -15,7 +15,7 @@ with open("inputs/input_day_02.txt", "r+") as file1:
             start, end = r.split("-")
             print(f"Start: {start}, End: {end}")
             for i in range(int(start), int(end)+1):
-                if is_invalid(i):
+                if is_invalid(i, len(str(i))):
                     print(f"Invalid number found: {i}")
                     result += i
 
