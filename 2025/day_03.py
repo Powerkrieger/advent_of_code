a78d5004ba9f49ce11e598d0b2a58241adfbad90
joost <joost.rehder@uni-rostock.de>
2025-12-03 06:42:54 +0100

    part 2 day 3,

--- 2025/day_03.py
+++ 2025/day_03.py
@@ -4,26 +4,25 @@ with open("inputs/input_day_03.txt", "r+") as file1:
         bank = line.strip()
         print(f"bank: {bank}, length: {len(bank)}")
         # find the highest number from the left until one less than the end and then the highest number from that to the end.
-        max_num = -1
+        batteries = []
         max_pos = -1
-        for i in range(len(bank)-1):
-            if int(bank[i]) > max_num:
-                max_num = int(bank[i])
-                max_pos=i
-                if max_num >= 9:
-                    break
-        print(f"Max num: {max_num} at pos {max_pos}, max_pos+1: {max_pos+1}")
-        # keep i as starting point and find the highest number from there to the end
-        max_second = -1
-        for j in range(max_pos+1, len(bank)):
-            print(f"Checking bank[{j}] = {bank[j]} against max_second {max_second}")
-            if int(bank[j]) > max_second:
-                max_second = int(bank[j])
-                if max_second >= 9:
-                    break
-        print(f"Max num: {max_num}, Max second: {max_second}")
-        bank_res = int(max_num) * 10 + int(max_second)
-        print(bank_res)
+        for each in range(11, -1, -1):
+
+            max_pos += 1
+            max_num = -1
+            # find highest number until len(bank)-each so that each-1 numbers are left for the second highest
+            for i in range(max_pos, len(bank)-each):
+                if int(bank[i]) > max_num:
+                    max_num = int(bank[i])
+                    max_pos=i
+                    if max_num >= 9:
+                        break
+            print(f"Max num: {max_num} at pos {max_pos}, max_pos+1: {max_pos+1}")
+            batteries.append(max_num)
+
+        # convert list of ints to one sequence of numbers and that sequence to one integer
+        bank_res = int(''.join(map(str, batteries)))
+        print(f"bank result: {bank_res}, batteries: {batteries}")
         result += bank_res
 
     print(result)
